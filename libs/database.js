0bd27b5b4d762d29274d045eead392534c1d3eb3
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-05-29 00:40:53 -0400

    Wrote own database function

--- libs/database.js
+++ libs/database.js
@@ -1,44 +1,23 @@
-"use strict";
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-var database = (function() {
-  var conn = null;
-  var init = function(config) {
-    console.log(
-      "Trying to connect to " +
-        config.host +
-        "/" +
-        config.database +
-        " MongoDB database"
-    );
-    var options = {
+const database = (() => {
+  const init = config => {
+    const connectionString = `mongodb://${config.host}/${config.database}`;
+    console.log(`Trying to connect to ${connectionString}`);
+    mongoose.connect(connectionString, {
       useNewUrlParser: true
-    };
+    });
 
-    var connString = `mongodb://${config.host}/${config.database}`;
-    mongoose.connect(connString, options);
-    conn = mongoose.connection;
-    conn.on("error", console.error.bind(console, "connection error:"));
-    conn.once("open", function() {
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "MongoDB Connection Error"));
+    db.once("open", function() {
       console.log("db connection open");
     });
-    return conn;
-  };
-
-  var close = function() {
-    if (conn) {
-      conn.close(function() {
-        console.log(
-          "Mongoose default connection disconnected through app termination"
-        );
-        process.exit(0);
-      });
-    }
+    return db;
   };
 
   return {
-    init,
-    close
+    init
   };
 })();
 
