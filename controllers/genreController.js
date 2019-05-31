b6a9bc4a46aef44c62bba4f6e6ef700f163fa5fe
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-05-31 11:44:23 -0400

    Add Genre page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,8 +1,15 @@
 const Genre = require("../models/genre");
 
 // Display list of all Genre.
-exports.genre_list = function(req, res) {
-  res.send("NOT IMPLEMENTED: Genre list");
+exports.genre_list = function(req, res, next) {
+  Genre.find().exec(function(err, list_genre) {
+    if (err) return next(err);
+    // success
+    res.render("genre_list", {
+      title: "Genre List",
+      genre_list: list_genre
+    });
+  });
 };
 
 // Display detail page for a specific Genre.
