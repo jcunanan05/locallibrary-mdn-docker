5abad62b64d8bc6d7426790560ca633792e8c578
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-05-31 18:24:24 -0400

    Add Add genre page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,6 +1,8 @@
 const Genre = require("../models/genre");
 const Book = require("../models/book");
 const async = require("async");
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 
 // Display list of all Genre.
 exports.genre_list = function(req, res, next) {
@@ -45,13 +47,44 @@ exports.genre_detail = function(req, res, next) {
 
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
-  res.send("NOT IMPLEMENTED: Genre create GET");
+  res.render("genre_form", { title: "Genre Form" });
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-};
+exports.genre_create_post = [
+  body("name", "Genre name required")
+    .isLength({ min: 1 })
+    .trim(),
+  sanitizeBody("name").escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    var genre = new Genre({
+      name: req.body.name
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("genre_form", {
+        title: "Create Genre",
+        genre,
+        errors: errors.array()
+      });
+      return;
+    } else {
+      Promise.resolve(await Genre.findOne({ name: req.body.name }))
+        .catch(err => next(err))
+        .then(found_genre => {
+          if (found_genre) {
+            res.redirect(found_genre.url);
+          } else {
+            genre.save(err => {
+              if (err) return next(err);
+              res.redirect(genre.url);
+            });
+          }
+        });
+    }
+  }
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
