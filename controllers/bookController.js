50823b9a776ed8d60cf69e929012d05a7ddf5476
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-09 00:07:01 -0400

    Add Book form logic and html form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,6 +2,8 @@ var Book = require("../models/book");
 var Author = require("../models/author");
 var Genre = require("../models/genre");
 var BookInstance = require("../models/bookinstance");
+const { body, validationResult } = require("express-validator/check");
+const { sanitizeBody } = require("express-validator/filter");
 
 var async = require("async");
 
@@ -72,14 +74,88 @@ exports.book_detail = function(req, res) {
 };
 
 // Display book create form on GET.
-exports.book_create_get = function(req, res) {
-  res.send("NOT IMPLEMENTED: Book create GET");
+exports.book_create_get = async function(req, res, next) {
+  Promise.all([await Author.find(), await Genre.find()])
+    .catch(err => next(err))
+    .then(([authors, genres]) => {
+      res.render("book_form", {
+        title: "Create Book",
+        authors,
+        genres
+      });
+    });
 };
 
 // Handle book create on POST.
-exports.book_create_post = function(req, res) {
-  res.send("NOT IMPLEMENTED: Book create POST");
-};
+exports.book_create_post = [
+  // Convert the genre to an array.
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      if (typeof req.body.genre === "undefined") req.body.genre = [];
+      else req.body.genre = new Array(req.body.genre);
+    }
+    next();
+  },
+
+  // Validate fields.
+  body("title", "Title must not be empty.")
+    .isLength({ min: 1 })
+    .trim(),
+  body("author", "Author must not be empty.")
+    .isLength({ min: 1 })
+    .trim(),
+  body("summary", "Summary must not be empty.")
+    .isLength({ min: 1 })
+    .trim(),
+  body("isbn", "ISBN must not be empty")
+    .isLength({ min: 1 })
+    .trim(),
+
+  // Sanitize fields (using wildcard).
+  sanitizeBody("*").escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    // Create a Book object with escaped and trimmed data.
+    var book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre
+    });
+
+    if (!errors.isEmpty()) {
+      Promise.all([await Author.find(), await Genre.find()])
+        .catch(err => next(err))
+        .then(([authors, genres]) => {
+          const checkedGenres = genres.map(genre => {
+            if (book.genre.indexOf(genre._id) > -1) {
+              return {
+                name: genre.name,
+                checked: true
+              };
+            }
+            return genre;
+          });
+          res.render("book_form", {
+            title: "Create Book",
+            authors,
+            genres: checkedGenres,
+            book,
+            errors: errors.array()
+          });
+          return;
+        });
+    } else {
+      Promise.resolve(await book.save())
+        .catch(err => next(err))
+        .then(() => {
+          res.redirect(book.url);
+        });
+    }
+  }
+];
 
 // Display book delete form on GET.
 exports.book_delete_get = function(req, res) {
